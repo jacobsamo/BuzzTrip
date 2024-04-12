e2d9fbf97bcf851fbbf8cd9282e342a339355d89
Jacob <jacob35422@gmail.com>
2024-04-12 19:15:44 +1000

    FEAT: posthog setup

--- src/components/shared/providers.tsx
+++ src/components/shared/providers.tsx
@@ -1,18 +1,29 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import env from "env";
+
+if (typeof window !== "undefined") {
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  });
+}
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        {children}
-        {/* {process.env.NODE_ENV === "development" && (
+      <PostHogProvider client={posthog}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+          {/* {process.env.NODE_ENV === "development" && (
           <ReactQueryDevtools initialIsOpen={false} />
         )} */}
-      </QueryClientProvider>
+        </QueryClientProvider>
+      </PostHogProvider>
     </>
   );
 };
