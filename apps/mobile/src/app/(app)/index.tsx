531d663f14740f395841d04145e4201840dc5463
Jacob <jacob35422@gmail.com>
2024-10-20 16:19:23 +1000

    fix: get types to work across api and mobile

--- apps/mobile/src/app/(app)/index.tsx
+++ apps/mobile/src/app/(app)/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { useAuth } from "@clerk/clerk-expo";
 import { Text, View } from "react-native";
+import { useQuery } from "@tanstack/react-query";
+import { apiClient } from "@/lib/api.client";
 
 // login page that after login will route to /[id] containing the userId
 export default function App() {
+  const { userId } = useAuth();
+
+  if (!userId) {
+    return <Text>Loading...</Text>;
+  }
+
+  const { data } = useQuery({
+    queryKey: ["maps", userId],
+    queryFn: async () => {
+      const res = await apiClient[":userId"].maps.$get({
+        param: { userId },
+      });
+      const data = await res.json();
+
+    },
+  });
+
   return (
     <View>
       <Text className="text-black">Main page</Text>
+      {data!}
     </View>
   );
 }
--- apps/mobile/src/components/providers/index.tsx
+++ apps/mobile/src/components/providers/index.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import ClerkProvider from "./clerk-provider";
-import { Text } from "react-native";
-import { ClerkLoaded } from "@clerk/clerk-expo";
+import ReactQueryProvider from "./react-query-provider";
+
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <SafeAreaView style={{ flex: 1 }}>{children}</SafeAreaView>
+      <ReactQueryProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            {children}
+          </GestureHandlerRootView>
+        </SafeAreaView>
+      </ReactQueryProvider>
     </ClerkProvider>
   );
 };
