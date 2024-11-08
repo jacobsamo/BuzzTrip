fdfb15ff91b501f9ab2e52f08a0daf87c4e342ed
Jacob <jacob35422@gmail.com>
2024-11-08 15:02:59 +1000

    fix: little fixes and improve on web and in db

--- apps/mobile/src/components/MapCards.tsx
+++ apps/mobile/src/components/MapCards.tsx
@@ -11,31 +11,14 @@ export interface MapCardProps {
 
 export const MapCard = ({ image, title, mapId }: MapCardProps) => {
   return (
-    <Link className="flex flex-1 items-center w-auto flex-col max-w-40 max-h-[300px]" href={`/map/${mapId}`}>
-      <Image src={image} alt="Map image" className="w-full h-full rounded-xl" />
+    <Link
+      className="max-h-[300px] w-auto max-w-40 flex-1 flex-col items-center shadow-md"
+      href={`/map/${mapId}`}
+    >
+      <Image src={image} alt="Map image" className="h-full w-full rounded-xl" />
       <Text className="text-center text-black">{title}</Text>
     </Link>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    width: "auto",
-    display: "flex",
-    flexDirection: "column",
-    maxWidth: 150,
-    maxHeight: 300,
-  },
-  image: {
-    width: 150,
-    height: 150,
-    borderRadius: 10,
-  },
-  title: {
-    color: "black",
-  },
-});
 
 export default MapCard;
