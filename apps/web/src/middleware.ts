8899ef80eb27d168344d0f232ae47d33f8c5acb7
Jacob <jacob35422@gmail.com>
2025-05-27 22:20:48 +1000

    HOT-FIX: middleware failing to build

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,11 +1,17 @@
-import { getSession } from "@/server/getSession";
+import type { Session } from "@buzztrip/api/auth";
+import { env } from "env";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
+  const session = (await fetch(`${env.NEXT_PUBLIC_API_URL}/auth/get-session`, {
+    headers: {
+      cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
+      Authorization: `Bearer ${env.NEXT_PUBLIC_API_SECRET_KEY}`,
+    },
+  }).then((res) => res.json())) as Session | null | undefined;
 
   if (!session) {
-    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+    return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
   return NextResponse.next();
