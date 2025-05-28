7e469718b3d63477145b96ecf48da89a5865036a
Jacob <jacob35422@gmail.com>
2025-05-28 13:06:23 +1000

    HOT-FIX: test using better fetch to get session instead

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,17 +1,23 @@
-import { getSessionCookie } from "better-auth/cookies";
-import { headers } from "next/headers";
+import { betterFetch } from "@better-fetch/fetch";
+import { env } from "env";
 import { NextRequest, NextResponse } from "next/server";
+import type { Session } from "./lib/auth-client";
 
 export async function middleware(request: NextRequest) {
-  const headerList = await headers();
-  const sessionCookie = getSessionCookie(headerList); // this handles getting the secure cookie __Secure-better-auth.
-  console.log("Trying to get session cookie:", {
-    sessionCookie,
-    headers: headerList,
-    request: request
-  });
+  const { data: session } = await betterFetch<Session>(
+    "/auth/get-session",
+    {
+      baseURL: env.NEXT_PUBLIC_API_URL,
+      headers: {
+        cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
+        Authorization: `Bearer ${env.NEXT_PUBLIC_API_SECRET_KEY}`,
+      },
+    }
+  );
 
-  if (!sessionCookie) {
+  console.log("Session", session);
+
+  if (!session) {
     return NextResponse.redirect(new URL("/auth/sign-in", request.url));
   }
 
