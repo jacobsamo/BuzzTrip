96726b36f3e8d213a63d2169152e440d8bee968d
Jacob <jacob35422@gmail.com>
2025-05-28 12:55:18 +1000

    HOT-FIX: middleware being stupid

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,11 +1,14 @@
 import { getSessionCookie } from "better-auth/cookies";
+import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request); // this handles getting the secure cookie __Secure-better-auth.
+  const headerList = await headers();
+  const sessionCookie = getSessionCookie(headerList); // this handles getting the secure cookie __Secure-better-auth.
   console.log("Trying to get session cookie:", {
     sessionCookie,
-    cookies: request.cookies,
+    headers: headerList,
+    request: request
   });
 
   if (!sessionCookie) {
