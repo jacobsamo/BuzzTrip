f1d9894aac972ff41a44d7ebde6b7e0bbc179eba
Jacob <jacob35422@gmail.com>
2025-09-27 13:08:59 +1000

    FIX: component library not working correctly

--- packages/components/src/ui/skeletons/sidebar-skeleton.tsx
+++ packages/components/src/ui/skeletons/sidebar-skeleton.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from "./skeleton";
+import { Skeleton } from "../skeleton";
 import React from "react";
 
 const SidebarSkeleton = ({ children }: { children?: React.ReactNode }) => {
