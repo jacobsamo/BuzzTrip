5efa49ed4aad0a71fb2a2f204dc2d9631bfb80e3
Jacob <jacob35422@gmail.com>
2025-10-11 15:45:20 +1000

    remove: mobile
    Currently we aren't using the mobile app and it will need a full rewrite
    anyways so may as well remove it at this stage, there is a backup branch
    that has the mobile app in it if needed

--- apps/mobile/src/lib/api.client.ts
+++
@@ -1,8 +0,0 @@
-import type { AppType } from "@buzztrip/api/src";
-import { hc } from "hono/client";
-
-export const apiClient = hc<AppType>(process.env.EXPO_PUBLIC_API_URL!, {
-  headers: {
-    Authorization: `Bearer ${process.env.EXPO_PUBLIC_API_SECRET_KEY}`,
-  },
-});
